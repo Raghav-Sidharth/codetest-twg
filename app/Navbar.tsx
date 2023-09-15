36fb5863c3d626ad71cc38c5054ac2c8f57755e7
Raghav Sidharth <raghav.sidharth@live.com>
2023-09-15 20:07:04 +1200

    Navbar not functional but displaying correctly

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,20 +4,33 @@ import Grid from '@mui/material/Grid'
 export default function Navbar() {
   return (
     <>
-      <div className="fixed w-full h-[80px] flex justify-between items-center px-4 ">
+      <div className="fixed w-full h-[80px] flex justify-between items-center p-5">
         <div>
           <Image
             src="./assets/Logo.svg"
             alt="Wassp Logo"
-            width="100"
-            height="100"
+            width="150"
+            height="150"
           />
         </div>
+
         {/* Menu */}
-        <ul className="hidden md:flex py-2">
+        <ul className="hidden md:flex px-2">
           <li className="px-2">SEARCH</li>
           <li className="px-2">LOGIN</li>
-          <li className="px-2">EN</li>
+          <li className="px-2 flex">
+            EN{' '}
+            <Image
+              className="p-1"
+              src="./assets/ArrowDown.svg"
+              alt="Menu"
+              width="25"
+              height="25"
+            />
+          </li>
+          <li className="px-2">
+            <Image src="./assets/Menu.svg" alt="Menu" width="25" height="25" />
+          </li>
         </ul>
       </div>
     </>
