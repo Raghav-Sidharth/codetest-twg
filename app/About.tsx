55721dea49f905322162d24bbfa2d83e5c9e6a42
Raghav Sidharth <raghav.sidharth@live.com>
2023-09-16 20:45:09 +1200

    About CSS changes

--- app/About.tsx
+++ app/About.tsx
@@ -3,9 +3,19 @@ import Image from 'next/image'
 export default function About() {
   return (
     <>
-      <div className="w-full bg-[#052133]  ">
-        <div className="bg-[#052133] ">
-          <Image src={'/Layers.svg'} alt={''} width="1000" height="1000" />
+      <div className="w-screen bg-[#052133] ">
+        <div className="bg-[#052133] flex ">
+          <div className="bg-[url('/Layers.svg')] bg-cover w-full max-h-[2000px] ">
+            {/* <Image src={'/Layers.svg'} alt={''} width="1000" height="1000" /> */}
+          </div>
+
+          <div>
+            <div className="font-norwester text-white text-6xl pt-[750px] pr-[100px]">
+              <h2 className="uppercase">
+                Welcome to the World of WASSP Multibeam
+              </h2>
+            </div>
+          </div>
         </div>
       </div>
     </>
