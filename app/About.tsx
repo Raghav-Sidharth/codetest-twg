2f55a0c49315e253f0617c038f539904b32436e1
Raghav Sidharth <raghav.sidharth@live.com>
2023-09-18 14:08:50 +1200

    Reworked styyling in About component

--- app/About.tsx
+++ app/About.tsx
@@ -2,38 +2,35 @@ import Image from 'next/image'
 
 export default function About() {
   return (
-    <>
-      <div className="w-screen bg-[#052133]">
-        <div className="bg-[#052133] grid grid-cols-2">
-          <div className="bg-center bg-cover w-full h-[2000px] bg-image"></div>
+    <div className="relative bg-[#052133] w-screen h-screen">
+      {/* Background image */}
+      <div className="absolute top-0 left-0 w-[1000px] h-full  bg-cover bg-image z-0"></div>
 
-          <div>
-            <div className="font-norwester text-white max-w-[800px] max-h-[950px] pt-[750px] ">
-              <h2 className="uppercase text-6xl max-w-[675px] max-h-[120px] ">
-                Welcome to the World of WASSP Multibeam
-              </h2>
-              <div className="flex flex-cols-3 flex-wrap pt-10 ">
-                <div className="max-w-[150px] max-h-[100px] pr-[100px]">
-                  <p>01</p>
-                </div>
-                <div className="max-w-[400px] max-h-[100px] pr-[110px]">
-                  <p> ABOUT WASSP</p>
-                </div>
-                <div className="max-w-[350px] max-h-[126px] font-haas">
-                  <p className="text-[16px]">
-                    WASSP, a powerful multi-beam sounder with 120-degree
-                    coverage and models for deep exploration, offers
-                    unparalleled accuracy, detail, and versatility. Target fish
-                    with steerable sounders, generate real-time 3D charts, map
-                    fish in water, and make better decisions with the
-                    user-friendly advanced mode.
-                  </p>
-                </div>
-              </div>
+      {/* Text overlay */}
+      <div className="relative h-full">
+        <div className="flex flex-cols flex-wrap pt-[500px] pl-[500px] pr-[500px] ml-[200px] mr-[200px]">
+          <h2 className="text-white text-6xl uppercase font-norwester">
+            Welcome to the World of WASSP Multibeam
+          </h2>
+          <div className="flex flex-cols-2 pt-10 font-norwester">
+            <div className="pr-10">
+              <p className="text-white">01</p>
+            </div>
+            <div className="pr-10">
+              <p className="text-white">ABOUT WASSP</p>
+            </div>
+            <div className="font-haas text-2xl">
+              <p className="text-white text-sm">
+                WASSP, a powerful multi-beam sounder with 120-degree coverage
+                and models for deep exploration, offers unparalleled accuracy,
+                detail, and versatility. Target fish with steerable sounders,
+                generate real-time 3D charts, map fish in water, and make better
+                decisions with the user-friendly advanced mode.
+              </p>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
