03c373724d73cca64213faa3707828a8bd34a1c4
Raghav Sidharth <raghav.sidharth@live.com>
2023-09-18 16:07:53 +1200

    made landing page a bit more responsive

--- app/LandingPage.tsx
+++ app/LandingPage.tsx
@@ -3,14 +3,12 @@ import Image from 'next/image'
 export default function LandingPage() {
   return (
     <>
-      <div className="landing-gradient h-screen text-white font-norwester ">
-        <div className="p-[120px]">{/* Padding Div */}</div>
-        <div className="pl-[50px]">
-          <div className=" pb-[20px]">
-            <h1 className="text-3xl">SEA IT ALL</h1>
+      <div className="landing-gradient h-screen text-white font-norwester flex items-center">
+        <div className="p-4 md:p-8 lg:p-16">
+          <div className="pb-4 md:pb-8 lg:pb-16">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl">SEA IT ALL</h1>
           </div>
-
-          <div className="text-7xl">
+          <div className="text-4xl md:text-5xl lg:text-6xl">
             <p className="pt-2">MULTIBEAM SONAR </p>
             <p className="pt-2">PRODUCTS & SOLUTIONS.</p>
           </div>
