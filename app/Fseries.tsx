b925805b9abac96ea60f755d8370d9769063a3a5
Raghav Sidharth <raghav.sidharth@live.com>
2023-09-18 18:45:19 +1200

    removed unnessecary lines of code

--- app/Fseries.tsx
+++ app/Fseries.tsx
@@ -2,19 +2,11 @@ export default function Fseries() {
   return (
     <>
       <div className="fishes-gradient h-screen flex flex-col justify-end items-start p-4">
-        {/* Line and numbers wrapper */}
         <div className="flex items-center">
-          {/* Number 01 */}
           <span className="text-lg text-white">01</span>
-
-          {/* Line */}
           <div className="flex-grow h-[1px] bg-white mx-2"></div>
-
-          {/* Number 03 */}
           <span className="text-lg text-white">03</span>
         </div>
-
-        {/* Text below the line */}
         <div className="text-white font-norwester uppercase text-8xl pt-2">
           <p className="text-lg">
             {'('}Fishing Series{')'}
--- app/LandingPage.tsx
+++ app/LandingPage.tsx
@@ -5,7 +5,6 @@ export default function LandingPage() {
     <>
       <div className="landing-gradient w-screen h-screen text-white font-norwester flex items-center">
         <div className="p-4 md:p-8 lg:p-16">
-          {/* Existing content */}
           <div className="pb-2 md:pb-4 lg:pb-4">
             <h1 className="text-2xl md:text-3xl lg:text-4xl">SEA IT ALL</h1>
           </div>
@@ -13,8 +12,6 @@ export default function LandingPage() {
             <p className="pt-2">MULTIBEAM SONAR </p>
             <p className="pt-2">PRODUCTS & SOLUTIONS.</p>
           </div>
-
-          {/* New content */}
           <div className="flex flex-cols-2 justify-center md:justify-start">
             <div className="text-left md:ml-[500px] md:mr-[1000px] lg:mt-[200px] pt-4 md:pt-8 lg:pt-16 font-haas">
               <p className="text-lg md:text-xl lg:text-2xl">
