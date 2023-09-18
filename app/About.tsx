4d86f605b36e2e3f8d337bdc6698cab3fbf82d0e
Raghav Sidharth <raghav.sidharth@live.com>
2023-09-18 16:23:04 +1200

    Made the content a bit more responsive

--- app/About.tsx
+++ app/About.tsx
@@ -4,23 +4,23 @@ export default function About() {
   return (
     <div className="relative bg-[#052133] w-screen h-screen">
       {/* Background image */}
-      <div className="absolute top-0 left-0 w-[1000px] h-full  bg-cover bg-image z-0"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-cover bg-image z-0"></div>
 
       {/* Text overlay */}
       <div className="relative h-full">
-        <div className="flex flex-cols flex-wrap pt-[450px] pl-[450px] pr-[450px] ml-[200px] mr-[200px]">
-          <h2 className="text-white text-6xl uppercase font-norwester">
+        <div className="flex flex-wrap lg:pt-[450px] lg:pl-[450px] lg:pr-[450px] lg:ml-[200px] lg:mr-[200px] pt-16 pl-8 pr-8">
+          <h2 className="text-white text-3xl pb-10 lg:text-6xl uppercase font-norwester">
             Welcome to the World of WASSP Multibeam
           </h2>
-          <div className="flex flex-cols-2 flex-grow pt-10 font-norwester">
-            <div className="pr-10">
+          <div className="flex pt-10 lg:pt-0 font-norwester">
+            <div className="pr-4 lg:pr-10">
               <p className="text-white">01</p>
             </div>
-            <div className="pr-10">
+            <div className="pr-4 lg:pr-10">
               <p className="text-white">ABOUT WASSP</p>
             </div>
-            <div className="font-haas text-2xl mr-[250px] ml-20">
-              <p className="text-white text-sm">
+            <div className="font-haas text-sm lg:text-xl mr-4 lg:mr-[250px] ml-4 lg:ml-20">
+              <p className="text-white">
                 WASSP, a powerful multi-beam sounder with 120-degree coverage
                 and models for deep exploration, offers unparalleled accuracy,
                 detail, and versatility. Target fish with steerable sounders,
