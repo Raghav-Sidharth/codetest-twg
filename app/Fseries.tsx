41cd7ac0d826bd11a8732e3b1b93f519d6bb109e
Raghav Sidharth <raghav.sidharth@live.com>
2023-09-19 15:14:31 +1200

    Added more content to Fseries Component

--- app/Fseries.tsx
+++ app/Fseries.tsx
@@ -2,16 +2,24 @@ export default function Fseries() {
   return (
     <>
       <div className="fishes-gradient h-screen flex flex-col justify-end items-start p-4">
-        <div className="flex items-center">
-          <span className="text-lg text-white">01</span>
-          <div className="flex-grow h-[1px] bg-white mx-2"></div>
-          <span className="text-lg text-white">03</span>
-        </div>
-        <div className="text-white font-norwester uppercase text-8xl pt-2">
-          <p className="text-lg">
-            {'('}Fishing Series{')'}
-          </p>
-          <p>F - Series</p>
+        <div className="flex justify-between items-end w-full">
+          <div className="text-white pt-2">
+            <div className="flex justify-between w-full font-norwester">
+              <span className="text-lg text-white pr-5">01</span>
+              <div className="h-[1px] w-full bg-white"></div>
+
+              <span className="text-lg text-white pl-5">03</span>
+            </div>
+            <p className="text-lg font-norwester uppercase">
+              {'('}Fishing Series{')'}
+            </p>
+            <p className="font-norwester uppercase text-8xl">F - Series</p>
+          </div>
+          <div className="text-white pt-2 mr-[100px] ml-[1000px] mb-[25px] font-haas">
+            <p className="pb-5 uppercase">F-Series</p>
+            <p className="">WASSP Multibeam finds fish and habitat over a</p>
+            <p>wider area than single-beam systems.</p>
+          </div>
         </div>
       </div>
     </>
