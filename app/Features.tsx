928fdf395c29e328977cb6af37e1bd0cd7be148a
Raghav Sidharth <raghav.sidharth@live.com>
2023-09-20 18:53:07 +1200

    data.json rendering into to Features component - adjusted styling to be similar to original design

--- app/Features.tsx
+++ app/Features.tsx
@@ -1,23 +1,6 @@
 'use client'
 import React, { useState } from 'react'
-
-const content = [
-  {
-    id: '01',
-    title: 'test title 1',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  },
-  {
-    id: '02',
-    title: 'test title 2 ',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  },
-  {
-    id: '03',
-    title: 'test title 3',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  },
-]
+import content from '../public/data.json'
 
 export default function Features() {
   const [selectId, setSelectId] = useState('01')
@@ -40,12 +23,12 @@ export default function Features() {
         {/* Right column for Buttons and Data section */}
         <div className="flex flex-col text-white w-2/3 items-start p-4 font-haas">
           {/* Buttons row */}
-          <div className="flex flex-row flex-wrap space-x-4 mb-10">
+          <div className="flex flex-row flex-wrap mb-10 ">
             {content.map((data) => (
               <div
                 key={data.id}
                 onClick={() => setSelectId(data.id)}
-                className={`cursor-pointer p-2 rounded-full border-2  ${
+                className={`cursor-pointer p-2 rounded-full border-2 pb-1 mb-5 mr-3 ${
                   selectId === data.id
                     ? 'bg-white text-[#04192b]'
                     : 'border-white text-white'
